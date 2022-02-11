26759f8e3fb153d8c0a5f246114a9bff8d1a383a
Mateusz <matijarzebski@wp.pl>
2022-02-11 13:56:11 +0100

    First working version

---
+++ RecrutationExcerciseNetWise/RecrutationExcerciseNetWise/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+//using Microsoft.AspNetCore.Mvc;
+//using RestSharp;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            string json = (new WebClient()).DownloadString("https://catfact.ninja/fact");
+
+            string path = @"testowy.txt";
+            StreamWriter sw;
+
+            if (!File.Exists(path))
+            {
+                sw = File.CreateText(path);
+                Console.WriteLine("created new file");
+            }
+            else
+            {
+                sw = new StreamWriter(path, true);
+            }
+            sw.WriteLine(json);
+            sw.Close();
+
+            Console.WriteLine(json);
+        }
+    }
+}
